92997d1acbb9942b122136e408b42d83cb41ffa8
vyasdhairya <70323148+vyasdhairya@users.noreply.github.com>
2022-01-08 12:08:33 +0530

    Update MainApp.py

--- MainApp.py
+++ MainApp.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import hashlib
-import cv2
-import numpy as np
 def make_hashes(password):
     return hashlib.sha256(str.encode(password)).hexdigest()
 def check_hashes(password,hashed_text):
@@ -34,7 +32,7 @@ def main():
     choice = st.sidebar.selectbox("Menu",menu)
     if choice == "Home":
         original_title="<p style='text-align: center;'>In this corporate world to deal with tedious work of higher numbers of tickets and bugs it is difficult task web and saas companies to manually work on it. So to overcome it we have developed web based application which automatically separates ticket and bug with the help of different machine learning techniques.</p>"
-        st.image(np.array([cv2.imread("flow.jpg")]), channels="BGR")
+        #st.image(np.array([cv2.imread("flow.jpg")]), channels="BGR")
         st.markdown(original_title, unsafe_allow_html=True)
     elif choice == "Login":
         st.subheader("Login Section")
